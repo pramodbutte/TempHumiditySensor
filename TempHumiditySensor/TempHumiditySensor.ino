d02252b71d1f4e17e66841320135ca3ffa0003e7
ButteP <ButteP@DWLA8114PB>
2017-04-04 22:43:26 -0700

    Add project files.

---
+++ TempHumiditySensor/TempHumiditySensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TempHumiditySensor.ino
+ Created:	4/4/2017 10:43:21 PM
+ Author:	ButteP
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
